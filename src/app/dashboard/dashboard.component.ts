54536215d324de47bcf65b41554880255051c293
Santosh Dumbre <Santosh.Dumbre@synechron.com>
2026-02-11 14:21:52 +0530

    highcharts angular integration issue fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import { HighchartsChartComponent } from 'highcharts-angular';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HighchartsChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+chartOptions1: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
+
+ chartOptions2: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
+
+ chartOptions3: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
+
+ chartOptions4: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
+
+ chartOptions5: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
+
+ chartOptions6: Highcharts.Options = { 
+  title: {
+    text: 'My Chart'
+  },
+  series: [{
+    type: 'line',
+    data: [1, 2, 3, 4, 5]
+  }]
+ }; 
 
 }
